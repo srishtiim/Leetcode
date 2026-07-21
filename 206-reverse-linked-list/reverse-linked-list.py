76b723d55df0ff51a8bc590bd5a11b116809f734
Srishti Mukherjee <133870723+srishtiim@users.noreply.github.com>
2026-07-21 14:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (30.66%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverseList(self, head):
+
+        prev = None
+        curr = head
+
+        while curr:
+
+            nxt = curr.next
+
+            curr.next = prev
+
+            prev = curr
+
+            curr = nxt
+
+        return prev
+        
